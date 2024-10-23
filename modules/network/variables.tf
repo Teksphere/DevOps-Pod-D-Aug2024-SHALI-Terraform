e442ddaabb67ac171b0837a73101fa4cb2a9f01b
ibk.teksphere <ibk.teksphere@gmail.com>
2024-10-23 16:08:40 +0100

    adding

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -24,13 +24,11 @@ variable "security_group_ids" {
 variable "name_prefix" {
   description = "Prefix for the instance name tag"
   type        = string
-  default = shali
 }
 
 variable "environment" {
   description = "Environment (sandbox, staging, production)"
   type        = string
-  default = prod
 }
 
 variable "tags" {
